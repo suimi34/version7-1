d34fd6c38927d287c36fabea0d8c81f0c801d0e7
Susumu Ishikawa <susumu2934@gmail.com>
2025-01-13 17:34:21 +0900

    add. smtp info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV["HOST"] }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "example.com",
+    user_name:       ENV["SMTP_USERNAME"],
+    password:        ENV["SMTP_PASSWORD"],
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
